336d55b11f020c3abd2a1222d06d910630bcd4fb
Владимир Тетерин <bigsinner@yandex.ru>
2021-12-21 05:42:10 +1000

    finver

--- CLion.cpp
+++ CLion.cpp
@@ -30,13 +30,16 @@ int main()
     float a, b, c;
     std::cout << "a, b, c:";
     std::cin >> a >> b >> c;
-
+  
+    if (a < 0) std::cout << "Not a quadratic equation!" << std::endl;
+    else if (a > 0)
+    {
         float discriminant = b * b - 4 * a * c;
         if (discriminant > 0)
         {
             float x1 = (-b + std::sqrt(discriminant)) / (2 * a);
             float x2 = (-b - std::sqrt(discriminant)) / (2 * a);
-                std::cout << "Root 1, 2: " << x1 << ", " << x2 << std::endl;
+            std::cout << "Root 1, 2: " << x1 << ", " << x2 << std::endl;
         }
         else if (discriminant == 0)
         {
@@ -47,5 +50,5 @@ int main()
         {
             std::cout << "Complex scenario is not supported!" << std::endl;
         }
-    
-}
+    }
+}   
